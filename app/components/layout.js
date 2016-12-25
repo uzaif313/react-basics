e58b108db350b587377a558c4158489bd90e8225
uzaif nilger <uzaif313@gmail.com>
2016-12-25 19:18:50 +0530

    init basic webpack and react seup

---
+++ app/components/layout.js
@@ -0,0 +1,17 @@
+ 
+ import React from 'react'
+
+ export default class Layout extends React.Component{
+
+    constructor(){
+        super();
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <h1>Main Layout</h1>
+            </div>
+        )
+    }
+ }
