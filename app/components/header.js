b72f8abacaeac4956a7cd325b8cf8d3adfd4f6b1
uzaif nilger <uzaif313@gmail.com>
2017-01-01 11:56:23 +0530

    implement stop watch

--- app/components/header.js
+++ app/components/header.js
@@ -18,13 +18,13 @@
 
 			<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		  	<ul className="nav navbar-nav">
-				<li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+				<li className="active"><a href="#">Stopwatch <span className="sr-only">(current)</span></a></li>
 				<li><a href="#">Link</a></li>
 			</ul>
-		  
+
 		</div>
 	  </div>
-	</nav>        
+	</nav>
 
 );
         }
--- app/components/stopwatch.js
+++ app/components/stopwatch.js
@@ -5,40 +5,34 @@
         constructor(){
             super();
             this.state = {
-                msec:0,
-                sec:0
+                milli_second:0,
+                second:0
             }
-            this.timer = undefined
+            this.timer = undefined;
         }
 
         render(){
             return (<div class="row">
                 <div class="col-md-6 col-md-offset-3">
                    <div class="col-sm-2">
-                        <input class="form-control"  value={this.state.msec}/>
+                        <input class="form-control"  value={this.state.milli_second}/>
                    </div>
                    <div class="col-md-2">
-                       <input class="form-control"  value={this.state.sec}/>
+                       <input class="form-control"  value={this.state.second}/>
                     </div>
                 </div>
-                    <hr/>
+                <hr/>
                 <div class="col-md-6 col-md-offset-3">
                     <div class="col-sm-2">
                         <button class="btn btn-success" onClick={this.startWatch.bind(this)}>Start</button>
                     </div>
                     <div class="col-sm-2">
-                        <button class="btn btn-danger">Stop</button>
+                        <button class="btn btn-info" onClick={this.pauseWatch.bind(this)}>Pause</button>
+                    </div>
+                    <div class="col-sm-2">
+                      <button class="btn btn-danger" onClick={this.reset.bind(this)}>Reset</button>
                     </div>
                 </div>
-
-                    <hr/>
-                <div class="col-md-6 col-md-offset-3">
-                    <div class="col-sm-2">
-                        <button class="btn btn-success" onClick={this.pauseWatch.bind(this)}>Start</button>
-                    </div>
-                    <div class="col-sm-2">
-                        <button class="btn btn-danger">Stop</button>
-                    </div>
 
 
             </div>)
@@ -49,25 +43,24 @@
        startWatch(){
            this.timer = setInterval(()=>{
                 var tsec = 0;
-                if(this.state.msec > 60){
+                if(this.state.milli_second > 60){
                  tsec = 0
-                 this.setState({sec:this.state.sec+1})
+                 this.setState({second:this.state.second+1})
                 }else{
-                  tsec = this.state.msec+1
+                  tsec = this.state.milli_second+1
 
                 }
-
-                this.setState({msec:tsec})
-            },10);
+                this.setState({milli_second:tsec})
+            },10)
         }
 
+        pauseWatch(){
+          clearInterval(this.timer)
+        }
 
-      pauseWatch() {
-
-            clearInterval(this.timer);
-
-       }
-
-
+        reset(){
+          clearInterval(this.timer)
+          this.setState({milli_second:0,second:0})
+        }
 
     }
